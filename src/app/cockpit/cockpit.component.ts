995acd011194890f713c2bda945ae006cf12e7f8
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-23 23:26:45 +0100

    Demonstration of using an alias for the  element name.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // EventEmitter lets us send out/emit these properties to make them accessible from
   // from outside of this component.
   // EventEmitter is a generic type that uses <>
+  // Addedd an alias for blueprintCreated so that only the alias is visible to the outside.
   newServerName = '';
   newServerContent = '';
 
