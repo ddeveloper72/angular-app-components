bca19b2351b8f372e3daa1bcd23de52f2a4ce48c
ddeveloper72 <duncan.niamh@gmail.com>
2019-05-23 22:31:49 +0100

    Added an alias name to our element input functiuon.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
   // by default, all properties of components are only accessible
   // inside these components, not from outside.
   // we need the @Input() to make the element accessible from the outside.
+  // using an alias helps us to specify the element targeted different from
+  // the property name.
   constructor() { }
 
   ngOnInit() {
